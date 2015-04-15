948111da54d07ad10dd03d09a40f836d7d5f34ba
Christian Persson <chrp@student.chalmers.se>
2015-04-15 16:33:08 +0200

    Adds JUnit and prepares for adding unit tests

---
+++ src/test/java/capstat/tests/ExampleTest.java
@@ -0,0 +1,13 @@
+package capstat.tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void someSimpleTest() {
+        assertEquals(1 + 1, 2);
+    }
+
+}
