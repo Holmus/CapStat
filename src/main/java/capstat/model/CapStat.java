939f3e0864cfa9ad62bebe85a638d77d7426f8e3
Christian Persson <chrp@student.chalmers.se>
2015-04-17 11:58:35 +0200

    Adds registration of new users

--- src/main/java/capstat/model/CapStat.java
+++ src/main/java/capstat/model/CapStat.java
@@ -19,4 +19,8 @@ public class CapStat {
     public boolean isNicknameValid(String nickname) {
         return this.userLedger.isNicknameValid(nickname);
     }
+
+    public void registerUser(String nickname, String name, String password, Birthday birthday, Admittance admittance) {
+        this.userLedger.registerUser(nickname, name, password, birthday, admittance);
+    }
 }
--- src/main/java/capstat/model/UserLedger.java
+++ src/main/java/capstat/model/UserLedger.java
@@ -1,8 +1,13 @@
 package capstat.model;
 
+import java.util.List;
+
+import capstat.utils.Security;
+
 public class UserLedger {
 
     private static UserLedger instance;
+    private List<User> users;
 
     private UserLedger() {
 
@@ -17,5 +22,17 @@ public class UserLedger {
 
     public boolean isNicknameValid(String nickname) {
         return nickname.matches("^[A-Za-zÅÄÖåäöü0-9 \\(\\)\\._\\-]+$");
+    }
+
+    public void registerUser(String nickname, String name, String password, Birthday birthday, Admittance admittance) {
+        ChalmersAge age = new ChalmersAge(birthday, admittance);
+        String hashedPassword = Security.hashPassword(password);
+        ELORanking ranking = ELORanking.defaultRanking();
+        this.createUserInDatabase(nickname, name, hashedPassword, age, ranking);
+    }
+
+    public void createUserInDatabase(String nickname, String name, String hashedPassword, ChalmersAge age, ELORanking ranking) {
+        User user = new User(nickname, name, hashedPassword, age, ranking);
+        users.add(user);
     }
 }
