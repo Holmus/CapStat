a9cff7c5ba8ffb33fef4c11accc4f370cfe55b7a
Christian Persson <chrp@student.chalmers.se>
2015-04-17 19:41:08 +0200

    Removes unnecessary imports

--- src/main/java/capstat/utils/Security.java
+++ src/main/java/capstat/utils/Security.java
@@ -2,9 +2,6 @@ package capstat.utils;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
-import java.io.UnsupportedEncodingException;
-import java.security.*;
-
 public abstract class Security {
 
     public static String hashPassword(String password) {
