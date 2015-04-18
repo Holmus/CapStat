c913732dbdab65c7b30b34126e0477f1804b91d1
Christian Persson <chrp@student.chalmers.se>
2015-04-18 16:08:19 +0200

    Adds initial (admittedly bad) documentation

--- src/main/java/capstat/model/Birthday.java
+++ src/main/java/capstat/model/Birthday.java
@@ -1,29 +1,54 @@
 package capstat.model;
 
+/**
+ * @author Christian Persson
+ */
 public class Birthday implements Comparable<Birthday> {
 
     private int year, month, day;
 
+    /**
+     * Creates a new Birthday instance.
+     * @param year year of birth
+     * @param month month of birth
+     * @param day day of birth
+     */
     public Birthday(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
+    /**
+     * Creates a new Birthday instance, as a copy of the given Birthday instance.
+     * @param birthday the Birthday instance to create a copy of
+     */
     public Birthday(Birthday birthday) {
         this.year = birthday.getYear();
         this.month = birthday.getMonth();
         this.day = birthday.getDay();
     }
 
+    /**
+     * Returns the year of birth.
+     * @return the year of birth
+     */
     public int getYear() {
         return this.year;
     }
 
+    /**
+     * Returns the month of birth.
+     * @return the month of birth
+     */
     public int getMonth() {
         return this.month;
     }
 
+    /**
+     * Returns the day of birth.
+     * @return the day of birth
+     */
     public int getDay() {
         return this.day;
     }
