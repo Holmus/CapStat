f8b3922d002a62f1b0f119c1b01f73a6016079ea
hjorthjort <rikard.hjort@gmail.com>
2015-04-21 13:46:10 +0200

    Adds method hit to Throw

--- src/main/java/capstat/utils/Throw.java
+++ src/main/java/capstat/utils/Throw.java
@@ -3,18 +3,27 @@ package capstat.utils;
 /**
  * @author hjorthjort
  */
-public class Throw {
+public abstract class Throw {
 
     private final static Throw HIT = new Hit();
     private final static Throw MISS = new Miss();
 
-    private static class Hit extends Throw {}
-    private static class Miss extends Throw {}
+    public abstract boolean hit();
 
-    public static Throw createHit() {
-        return HIT;
+    private static class Hit extends Throw {
+        @Override
+        public boolean hit() {
+            return true;
+        }
+    }
+    private static class Miss extends Throw {
+        @Override
+        public boolean hit() {
+            return false;
+        }
     }
 
+    public static Throw createHit() { return HIT; }
     public static Throw createMiss() {
         return MISS;
     }
