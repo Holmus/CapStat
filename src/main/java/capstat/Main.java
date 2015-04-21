b58e99e728f689f07c446bd40a72ef12bab61450
Jakob Holmgren <jholmgren95@gmail.com>
2015-04-21 13:47:30 +0200

    Added the possibility to see state of glasses visually

--- src/main/java/capstat/Main.java
+++ src/main/java/capstat/Main.java
@@ -12,51 +12,63 @@ import java.util.Scanner;
 
 public class Main{
 
-    GameFactory factory = new GameFactory();
-    Match test = factory.createDefaultMatch();
-    Scanner sc = new Scanner(System.in);
-    String s = sc.nextLine();
-
+    static GameFactory factory = new GameFactory();
+    static Match testMatch = factory.createDefaultMatch();
+    static Scanner sc = new Scanner(System.in);
+    static User player1 = new User();
+    static User player2 = new User();
     public static void main(String [] args){
 
-
-        test.startMatch();
-        User player1 = test.setPlayer1();
-        User player2 = test.setPlayer2();
-        while(!s.equals("q")){
-            User curr = test.getPlayerNextUp();
+        String s;
+        testMatch.startMatch();
+        testMatch.setPlayer1(player1);
+        testMatch.setPlayer2(player2);
+        do {
+            s = sc.nextLine();
+        } while(!s.equals("q")){
+            User currPlayer = testMatch.getPlayerNextUp();
             switch(s){
-                case h: updateState(true,curr);
-                        TurnPrinter(true,curr);
-                        break;
-                case m: updateState(false, curr);
-                        TurnPrinter(false, curr);
-                        break;
+                case "h": TurnPrinter(true, currPlayer);
+                          break;
+                case "m": TurnPrinter(false, currPlayer);
+                          break;
             }
-
+            s = sc.nextLine();
         }
         System.exit(0);
     }
     //TODO: glassState
     public String TurnPrinter(boolean hit, User currPlayer){
         char star = '*';
-
-        if(currPlayer.equals(player 1)){
+        updateState(hit, currPlayer);
+        if(currPlayer.equals(player1)){
             System.out.println(star + glassState());
         }
-        if(currPlayer.equals(player 2)){
+        if(currPlayer.equals(player2)){
             System.out.println(glassState() + star);
         }
 
     }
     public void updateState(boolean hit, User currPlayer){
         if(hit == true){
-            test.recordHit();
+            testMatch.recordHit();
             System.out.println(currPlayer.getName() + ": hit!");
         } else if(hit == false){
-            test.recordMiss();
+            testMatch.recordMiss();
             System.out.println(currPlayer.getName() + ": miss!");
         }
 
+    }
+    public String glassState(){
+        Match.Glass[] glasses = testMatch.getGlasses();
+        String returnString = "";
+        for(int i=0; i<glasses.length; i++){
+            if(glasses[i].glassIsActive()){
+                returnString = returnString + "o";
+            } else if(glasses[i].glassIsActive()){
+                returnString = returnString + "x";
+            }
+        }
+        return returnString;
     }
 }
