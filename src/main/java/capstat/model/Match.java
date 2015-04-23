148ccf6a1b03e4bba0aeba930528b9f1f755aff7
hjorthjort <rikard.hjort@gmail.com>
2015-04-23 03:14:06 +0200

    BUGFIX: Player 1 score calculation would cause exception

--- src/main/java/capstat/model/Match.java
+++ src/main/java/capstat/model/Match.java
@@ -156,7 +156,7 @@ public class Match {
 
     public int getPlayer1Score() {
         int score = 0;
-        for (int i = this.glasses.length-1; i >= this.glasses.length/2; i++) {
+        for (int i = this.glasses.length-1; i >= this.glasses.length/2; i--) {
             if (this.glasses[i].isActive()) break;
             else score++;
         }
