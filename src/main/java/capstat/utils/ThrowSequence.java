2cd71f5e7a926c441f3c7c871eb5f6a25aabc571
hjorthjort <rikard.hjort@gmail.com>
2015-04-24 13:17:52 +0200

    Creates method for updating recording state

--- src/main/java/capstat/utils/ThrowSequence.java
+++ src/main/java/capstat/utils/ThrowSequence.java
@@ -23,6 +23,24 @@ public class ThrowSequence {
         this.currentSequence.add(newThrow);
     }
 
+    /**
+     *
+     * @param glassesState an array of glasses, representing the state of the
+     *                     game.
+     * @param startingPlayerState an integer, 1 or 2, representing which
+     *                            player starts.
+     * @throws IllegalArgumentException if glassesState does not represent a
+     * valid state, or startingPlayerState is not 1 or 2.
+     */
+    public void updateRecordState(Match.Glass[] glassesState, int
+            startingPlayerState) {
+        //TODO Implement throwing exceptions, with method for checking if a
+        // glasses state is valid.
+        this.sequences.add(currentSequence);
+        this.currentSequence = new PartialSequence(glassesState,
+                startingPlayerState);
+    }
+
     private class PartialSequence {
         private Match.Glass[] glasses;
         private int startingPlayer;
