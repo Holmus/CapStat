906d2dcacb242a235e631214fd1eafcc7a267276
hjorthjort <rikard.hjort@gmail.com>
2015-05-09 01:00:40 +0200

    Adds more methods to UserQueryHelper interface

--- src/main/java/capstat/infrastructure/UserQueryHelper.java
+++ src/main/java/capstat/infrastructure/UserQueryHelper.java
@@ -2,11 +2,18 @@ package capstat.infrastructure;
 
 import capstat.model.User;
 
+import java.util.List;
+
 /**
  * @author hjorthjort
  */
 public interface UserQueryHelper {
-    void addUserToDatabase(User dummy);
 
+    void addUserToDatabase(User dummy);
     UserDatabaseRow getUserByNickname(String nickname);
+    UserDatabaseRow getUserByName(String name);
+    List<UserDatabaseRow> getUsersByAdmittance(int year, int readingPeriod);
+    List<UserDatabaseRow> getUsersByNicknameMatch(String regex);
+    List<UserDatabaseRow> getUsersByNameMatch(String regex);
+    List<UserDatabaseRow> getUsersInELORankRange(int minELO, int maxELO);
 }
