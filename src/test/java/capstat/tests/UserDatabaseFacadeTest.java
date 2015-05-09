bc08e86e24d01a5fdfc018da7b3122e3c6428261
hjorthjort <rikard.hjort@gmail.com>
2015-05-09 12:29:48 +0200

    Creates unit test for fetching single user from database

--- src/test/java/capstat/tests/UserDatabaseFacadeTest.java
+++ src/test/java/capstat/tests/UserDatabaseFacadeTest.java
@@ -3,8 +3,7 @@ package capstat.tests;
 import capstat.infrastructure.DatabaseQueryFactory;
 import capstat.infrastructure.UserDatabaseRow;
 import capstat.infrastructure.UserQueryHelper;
-import capstat.model.User;
-import capstat.model.UserFactory;
+import capstat.model.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -24,10 +23,26 @@ public class UserDatabaseFacadeTest {
 
     @Test
     public void getUserTest() {
-        User dummy = UserFactory.createDummyUser1();
-        userdb.addUserToDatabase(dummy);
-        UserDatabaseRow u = userdb.getUserByNickname(dummy.getNickname());
-        //TODO make sure User fields match UserDatabaseRowFields
+        User dummyUser = UserFactory.createDummyUser1();
+        userdb.addUserToDatabase(dummyUser);
+        UserDatabaseRow userDBRow = userdb.getUserByNickname(dummyUser.getNickname());
+        String standardMessage = "Make sure the stored users fields and the database " +
+                "fields contain the same information: ";
+        assertEquals(standardMessage + "Nickname ", dummyUser.getNickname(), userDBRow.nickname);
+        assertEquals(standardMessage + "Name", dummyUser.getName(), userDBRow.name);
+        assertEquals(standardMessage + "Hashed password", dummyUser.getHashedPassword(), userDBRow
+                .hashedPassword);
+        //Create a new ChalmersAge from the fields in the database row object
+        ChalmersAge userDBChalmersAge = new ChalmersAge(
+                new Birthday(userDBRow.birthdayYear, userDBRow.birthdayMonth,
+                        userDBRow.birthdayDay),
+                new Admittance(userDBRow.admittanceYear,
+                        userDBRow.admittanceReadingPeriod)
+        );
+        assertEquals(standardMessage + "Chalmers age", dummyUser
+                .getChalmersAge(), userDBChalmersAge);
+        assertEquals(standardMessage + "ELORanking", dummyUser.getRanking(),
+                new ELORanking(userDBRow.ELORanking));
     }
 
 }
