384b1ddcc8019d53f737c6333131a73ce77b58c2
hjorthjort <rikard.hjort@gmail.com>
2015-05-10 21:46:22 +0200

    Implements EventBus adder methods

--- src/main/java/capstat/infrastructure/EventBus.java
+++ src/main/java/capstat/infrastructure/EventBus.java
@@ -2,6 +2,7 @@ package capstat.infrastructure;
 
 import java.awt.*;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -26,8 +27,19 @@ public class EventBus {
         return instance;
     }
 
+    /**
+     * Adds a listener who will be notified when an event with the specified
+     * String as key has taken place. If the listener already is registered,
+     * this method has no effect.
+     * @param event the string that subject will notify by and will be
+     *              passed to the listeners notify method
+     * @param listener the listener that should be notified
+     */
     public void addNotifyEventListener(final String event, final NotifyEventListener listener) {
-
+        if (!this.notifyListenersMap.containsKey(event)) {
+            this.notifyListenersMap.put(event, new HashSet<>());
+        }
+        this.notifyListenersMap.get(event).add(listener);
     }
 
     public void removeNotifyEventListener(final String event, final NotifyEventListener listener) {
@@ -38,8 +50,20 @@ public class EventBus {
 
     }
 
+    /**
+     * Adds a listener who will be notified when an event with the specified
+     * String as key has taken place. If the listener already is registered,
+     * this method has no effect.
+     * @param event the string that subject will notify by and will be
+     *              passed to the listeners dataNotify method
+     * @param listener the listener that should be notified
+     */
     public void addDataEventListener(final
                                      String event, final DataEventListener listener) {
+        if (!this.dataListenersMap.containsKey(event)) {
+            this.dataListenersMap.put(event, new HashSet<>());
+        }
+        this.dataListenersMap.get(event).add(listener);
 
     }
 
