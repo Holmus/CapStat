a618b9a477eeded05b07069183d47b8a72ba9c24
lol2kpe <johandi@student.chalmers.se>
2015-05-11 10:27:20 +0200

    Created DatabaseTest, rewrote DatabaseConnection to handle incoming querys w. methods: insert,alter,read.

--- src/main/java/capstat/utils/DatabaseConnection.java
+++ src/main/java/capstat/utils/DatabaseConnection.java
@@ -1,54 +1,71 @@
 package capstat.utils;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jibbs on 07/05/15.
  */
 public class DatabaseConnection {
-	public static void main(String[] args) {
+
+	public Connection conn;
+
+	public DatabaseConnection() {
+		this.conn = null;
 		try {
 			String url = "jdbc:mysql://localhost:3306/capstat";
 			String userName = "capstat_user"; // Your username goes here!
 			String password = "1234"; // Your password goes here!
-			Connection conn = DriverManager.getConnection("" + url + "?user=" + userName + "&password=" + password);
-			readUsers(conn);
+			this.conn = DriverManager.getConnection("" + url + "?user=" + userName + "&password=" + password);
 		} catch (SQLException e) {
 			System.err.println(e);
 			System.exit(2);
 		}
 	}
 
+	public void insert(String statement){
+		Statement myStmt;
+		try {
+			myStmt = conn.createStatement();
+			myStmt.executeUpdate(statement);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-	private static void readUsers(Connection conn) {
+	public void alter(String statemenet) {
+
+		//TODO
+	}
+
+	public List<String> read(String statement) {
 
 		Statement myStmt;
 		try {
 			myStmt = conn.createStatement();
-		    /* Checks and tells if the student got unregistered from a course or was already not registered */
-
-			//myStmt.executeUpdate("INSERT INTO Users (nick, name, pass, chalmersAge, iloRank) VALUES ('lol2kpe', 'Johan Andersson', 'lol', 912, 'Master')");
-			myStmt.executeUpdate("INSERT INTO Users (nick, name, pass, chalmersAge, iloRank) VALUES ('user1', 'Arne Ranta', 'lol2', 123, 'n00b')");
-			myStmt.executeUpdate("INSERT INTO Users (nick, name, pass, chalmersAge, iloRank) VALUES ('user2', 'Ben Dover', 'lol4', 1212, 'n00b')");
-			ResultSet rs = myStmt.executeQuery("SELECT * FROM Users");
+			ResultSet rs = myStmt.executeQuery(statement);
+			List<String> resultList = new ArrayList<>();
 			while ( rs.next() ) {
 				String nick = rs.getString(1);
 				String name = rs.getString(2);
 				String pass = rs.getString(3);
 				String chalmersAge = rs.getString(4);
 				String iloRank = rs.getString(5);
-				System.out.println("" + nick + "  " + name + "  " + pass + "  " + chalmersAge + "  " + iloRank);
+				resultList.add("" + nick + "  " + name + "  " + pass + "  " + chalmersAge + "  " + iloRank);
 			}
 
 			myStmt.close(); /* Closes the statement when everything is done */
+
+			return resultList;
+
 		} catch (SQLException se) {
 			System.out.println("" + se.getMessage());
 		} catch (Exception e) {
 			//Handle errors for Class.forName
 			e.printStackTrace();
 		}
-
+		return null;
 	}
-
 
 }
