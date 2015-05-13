57fa25c08282b8534a4615d6a7340cdcc3ede66c
lol2kpe <johandi@student.chalmers.se>
2015-05-13 12:09:02 +0200

    added previeleges to mysql.proc to be used by  jooq

--- database/capstat.sql
+++ database/capstat.sql
@@ -26,6 +26,7 @@ DROP DATABASE capstat;
 CREATE USER 'capstat_user'@'localhost' IDENTIFIED BY '1234';
 CREATE DATABASE capstat;
 GRANT ALL ON capstat.* TO 'capstat_user'@'localhost';
+GRANT ALL PRIVILEGES ON mysql.proc TO 'capstat_user'@'localhost'
  */
 
 
