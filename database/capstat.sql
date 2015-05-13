89f538318b1c49ad93191707a7a7b29af46ce9dd
lol2kpe <johandi@student.chalmers.se>
2015-05-13 14:08:54 +0200

    fixed previeleges code in capstat.sql

--- database/capstat.sql
+++ database/capstat.sql
@@ -21,12 +21,13 @@ end;
 
 /*
 DROP DATABASE capstat;
-*/
-/*
+DROP USER 'capstat_user'@'localhost';
+
 CREATE USER 'capstat_user'@'localhost' IDENTIFIED BY '1234';
 CREATE DATABASE capstat;
+
 GRANT ALL ON capstat.* TO 'capstat_user'@'localhost';
-GRANT ALL PRIVILEGES ON mysql.proc TO 'capstat_user'@'localhost'
+GRANT ALL PRIVILEGES ON proc.* TO 'capstat_user'@'localhost';
  */
 
 
