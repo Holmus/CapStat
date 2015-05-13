67e95c6b3eef982ff5b59d7aa120ad45b46f90af
lol2kpe <johandi@student.chalmers.se>
2015-05-13 11:56:46 +0200

    Jooq is now set up and is working. * DatabaseConnection: Removed methods to manipulate db directly via jdbc. * DatabaseTest: Added and changed a few tests to use jooq instead.

--- src/main/java/capstat/utils/DatabaseConnection.java
+++ src/main/java/capstat/utils/DatabaseConnection.java
@@ -4,12 +4,9 @@ import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.jooq.generated.db.*;
-import org.jooq.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Created by jibbs on 07/05/15.
@@ -23,22 +20,21 @@ public class DatabaseConnection {
 	private String userName;
 	private String password;
 
+	/**
+	 * Sets the connection up to both jdbc and jooq.
+	 */
 	public DatabaseConnection() {
 		this.conn = null;
 		this.url = "jdbc:mysql://localhost:3306/capstat";
 		this.userName = "capstat_user"; // Your username goes here!
 		this.password = "1234"; // Your password goes here!
-
-		try {
-			this.conn = DriverManager.getConnection("" + url + "?user=" + userName + "&password=" + password);
-			database = DSL.using(conn, SQLDialect.MYSQL);
-			this.conn.close();
-		} catch (SQLException e) {
-			System.err.println(e);
-			System.exit(2);
-		}
+		connect();
+		database = DSL.using(conn, SQLDialect.MYSQL);
 	}
 
+	/**
+	 * Opens a jdbc connection to database.
+	 */
 	public void connect() {
 		try {
 			this.conn = DriverManager.getConnection("" + url + "?user=" + userName + "&password=" + password);
@@ -48,65 +44,15 @@ public class DatabaseConnection {
 		}
 	}
 
+	/**
+	 * Closes the jdbc connection.
+	 */
 	public void disconnect() {
 		try {
 			this.conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-
-	public void insert(String statement){
-		Statement myStmt;
-		try {
-			myStmt = conn.createStatement();
-			myStmt.executeUpdate(statement);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void alter(String statement) {
-
-		//TODO
-
-		Statement myStmt;
-		try {
-			myStmt = conn.createStatement();
-			myStmt.executeUpdate(statement);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public List<String> read(String statement) {
-
-		Statement myStmt;
-		try {
-			myStmt = conn.createStatement();
-			ResultSet rs = myStmt.executeQuery(statement);
-			List<String> resultList = new ArrayList<>();
-			while ( rs.next() ) {
-				String nick = rs.getString(1);
-				String name = rs.getString(2);
-				String pass = rs.getString(3);
-				String chalmersAge = rs.getString(4);
-				String iloRank = rs.getString(5);
-				resultList.add("" + nick + "  " + name + "  " + pass + "  " + chalmersAge + "  " + iloRank);
-			}
-
-			myStmt.close(); /* Closes the statement when everything is done */
-
-			return resultList;
-
-		} catch (SQLException se) {
-			System.out.println("" + se.getMessage());
-		} catch (Exception e) {
-			//Handle errors for Class.forName
-			e.printStackTrace();
-		}
-		return null;
 	}
 
 }
--- src/test/java/capstat/tests/DatabaseTest.java
+++ src/test/java/capstat/tests/DatabaseTest.java
@@ -1,88 +1,112 @@
 package capstat.tests;
 
-import static org.junit.Assert.*;
 import capstat.utils.DatabaseConnection;
 import org.jooq.DSLContext;
-import org.jooq.impl.DSL;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.generated.db.capstat.tables.Users;
+import org.jooq.generated.db.capstat.tables.records.UsersRecord;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Iterator;
-import java.util.List;
-import org.jooq.*;
-
 /**
  * Created by jibbs on 09/05/15.
+ *
+ * Performs a number of tests to manipulate the database in several
+ * different ways using the jooq library.
  */
 public class DatabaseTest {
 
-	private DatabaseConnection db;
 
+	private DatabaseConnection dbConn;
+	private DSLContext db;
+
+	/**
+	 * Setting up the connection using the DatabaseConnection-class.
+	 */
 	@Before
 	public void initiateConnection() {
-		db = new DatabaseConnection();
-		db.connect();
+		this.dbConn = new DatabaseConnection();
+		this.db = dbConn.database;
 	}
 
-	@Test
-	public void useJooq() {
-//		DSLContext create = DSL.using(mySql);
-	}
-
-
+	/**
+	 * Deletes all users.
+	 */
 	@Test
 	public void deleteAllUsers() {
-		db.alter("DELETE FROM Users");
+		db.deleteFrom(Users.USERS).execute();
 	}
 
+	/**
+	 * Inserts three users.
+	 */
 	@Test
 	public void insertUsers () {
-		db.insert("INSERT INTO Users (nick, name, pass, chalmersAge, iloRank) VALUES ('lol2kpe', 'Johan Andersson', 'lol', 913, 'Master')");
-		db.insert("INSERT INTO Users (nick, name, pass, chalmersAge, iloRank) VALUES ('user1', 'Arne Ranta', 'lol2', 123, 'n00b')");
-		db.insert("INSERT INTO Users (nick, name, pass, chalmersAge, iloRank) VALUES ('user2', 'Ben Dover', 'lol4', 1212, 'n00b')");
+		db.insertInto(Users.USERS, Users.USERS.NICK, Users.USERS.NAME, Users.USERS.PASS, Users.USERS.CHALMERSAGE, Users.USERS.ILORANK)
+				.values("lol2kpe", "Johan Andersson", "lol", 913, "Master" ).execute();
+		db.insertInto(Users.USERS, Users.USERS.NICK, Users.USERS.NAME, Users.USERS.PASS, Users.USERS.CHALMERSAGE, Users.USERS.ILORANK)
+				.values("user1", "Arne Ranta", "lol2", 123, "n00b" ).execute();
+		db.insertInto(Users.USERS, Users.USERS.NICK, Users.USERS.NAME, Users.USERS.PASS, Users.USERS.CHALMERSAGE, Users.USERS.ILORANK)
+				.values("user2", "Ben Dover", "lol4", 347, "Mediocre" ).execute();
 	}
 
-
+	/**
+	 * Prints the users in the database in several different ways.
+	 */
 	@Test
 	public void getUsers() {
-		Iterator<String> resultIterator;
-		resultIterator = db.read("SELECT * FROM Users").iterator();
-		while ( resultIterator.hasNext() ) {
-			System.out.println("" + resultIterator.next());
+		Result<Record> result = db.select().from(Users.USERS).fetch();
+		System.out.println("\nLISTING ALL USERS VIA RESULT : \n" + result + "\n");
+
+		int count = 0;
+		for ( UsersRecord userRecord : db.selectFrom(Users.USERS).fetch() ) {
+			count++;
+			System.out.print("\nPRINTING USER " + count + " VIA USERRECORD :\n" + userRecord + "\n");
 		}
+
+		System.out.println ( "\nPRINTING SECOND USERS NICK USING RESULT.GETVALUE: " + result.getValue(1,"nick") + "\n");
 	}
 
-
+	/**
+	 * Inserts matches.
+	 */
 	@Test
 	public void insertMatches() {
 
 	}
 
-
+	/**
+	 * Prints the matches.
+	 */
 	@Test
 	public void getMatches() {
 
 	}
 
-
+	/**
+	 * Inserts throw sequences.
+	 */
 	@Test
 	public void insertThrowSequences() {
 
 	}
 
-
+	/**
+	 * Prints everything currently in the database.
+	 */
 	@Test
 	public void getAll() {
 
 	}
 
+	/**
+	 * Closes the connection to the database.
+	 */
 	@After
 	public void closeConn() {
-		db.disconnect();
+		dbConn.disconnect();
 	}
 
 }
