a36d40f3f31ccb067ce175e1713c35a64de0e94c
lol2kpe <johandi@student.chalmers.se>
2015-05-18 20:57:54 +0200

    Created a new class: MatchDatabaseHelper

---
+++ src/main/java/capstat/infrastructure/MatchDatabaseHelper.java
@@ -0,0 +1,16 @@
+package capstat.infrastructure;
+
+import java.util.Set;
+
+/**
+ * @author jibbs
+ * An interface for a Facade used to get and add users from the database.
+ */
+public interface MatchDatabaseHelper {
+
+	/**
+	 * Adds this user to the database. If one already exists, overwrite it.
+	 * @param user
+	 */
+
+}
