4ca2a96d0944470f78a1d8f0c9d6c93257e64cf5
lol2kpe <johandi@student.chalmers.se>
2015-05-19 13:39:13 +0200

    Changed previleges for jooq to access mysql.prog

--- database/capstat.sql
+++ database/capstat.sql
@@ -27,7 +27,7 @@ CREATE USER 'capstat_user'@'localhost' IDENTIFIED BY '1234';
 CREATE DATABASE capstat;
 
 GRANT ALL ON capstat.* TO 'capstat_user'@'localhost';
-GRANT ALL PRIVILEGES ON proc.* TO 'capstat_user'@'localhost';
+GRANT ALL PRIVILEGES ON mysql.proc TO 'capstat_user'@'localhost';
  */
 
 
