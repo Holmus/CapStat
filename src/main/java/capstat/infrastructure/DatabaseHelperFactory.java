8593c8700a3513253d3bb37476becf74fe9ba5c4
hjorthjort <rikard.hjort@gmail.com>
2015-05-19 10:40:53 +0200

    Adds documentation to DatabaseHelperFactory

--- src/main/java/capstat/infrastructure/DatabaseHelperFactory.java
+++ src/main/java/capstat/infrastructure/DatabaseHelperFactory.java
@@ -1,6 +1,14 @@
 package capstat.infrastructure;
 
 /**
+ * A class for dependency injection of the facades for interacting with the
+ * database.
+ *
+ * This class returns objects for interacting with the database. The static
+ * type of the objects is an interface. This decouples the rest of the system
+ * from the database implementation, which may thus be migrated, as long as
+ * the interface is satisfied.
+ *
  * @author hjorthjort
  */
 public class DatabaseHelperFactory {
