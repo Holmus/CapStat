68b1648565d676827571d99b6c7b7edc65792270
Christian Persson <chrp@student.chalmers.se>
2015-05-19 15:00:44 +0200

    Restructures unit testing to be perhaps a little more easy to work with

--- src/test/java/capstat/tests/SingleMatchCalculatorTest.java
+++ src/test/java/capstat/tests/SingleMatchCalculatorTest.java
@@ -27,15 +27,13 @@ public class SingleMatchCalculatorTest {
         SingleMatchCalculator smc;
 
         // A test using a single partial sequence
-        PartialSequenceBlueprint psb = new PartialSequenceBlueprint(
+        psbs.add(new PartialSequenceBlueprint(
             new boolean[] { true, true, true, true, true, true, true },
             1,
             false,
             new boolean[] { false, false, false, false, false, true, true, true, false, false, true, true, true, false, false, false, true, false, false, true, true, true, true, false, false, false, false, false, true, true, false, false, true, true, true, true, false, false, false, true, true, true, true, true, true, true, false, true, true, true, true, false, false, false, false, false, false, false, true, true, true, false, false, true, true, true, false, false, false, false, false, false, false, true, true, true, true, true, false, false, false, true, true, true, true, true, true, true, false }
-        );
+        ));
 
-        psbs.clear();
-        psbs.add(psb);
         mrb = new MatchResultBlueprint(
             -1,
             "Dummy 1",
@@ -56,6 +54,8 @@ public class SingleMatchCalculatorTest {
 
         assertEquals("Player 1 accuracy for match should be around 46,81%", 0.4681, player1Accuracy, 0.01);
         assertEquals("Player 2 accuracy for match should be around 58,14%", 0.5814, player2Accuracy, 0.01);
+
+        psbs.clear();
 
         // ---------------------------------------------------------------------
 
@@ -64,49 +64,43 @@ public class SingleMatchCalculatorTest {
         // beginning of a duel and ends with a miss that ends a duel
 
         // The first two duels in the first partial sequence
-        PartialSequenceBlueprint psb1 = new PartialSequenceBlueprint(
+        psbs.add(new PartialSequenceBlueprint(
             new boolean[] { true, true, true, true, true, true, true },
             1,
             false,
             new boolean[] { false, false, false, false, false, true, true, true, false, false, true, true, true, false }
-        );
+        ));
         // Duels 3 (inclusive) to 5 (inclusive)
-        PartialSequenceBlueprint psb2 = new PartialSequenceBlueprint(
+        psbs.add(new PartialSequenceBlueprint(
             new boolean[] { false, false, true, true, true, true, true },
             1,
             false,
             new boolean[] { false, false, true, false, false, true, true, true, true, false, false, false, false, false, true, true, false }
-        );
+        ));
         // Duel 5 ended the match, so a new round is initiated (hence all glasses being active)
 
         // Duel 6
-        PartialSequenceBlueprint psb3 = new PartialSequenceBlueprint(
+        psbs.add(new PartialSequenceBlueprint(
             new boolean[] { true, true, true, true, true, true, true },
             1,
             false,
             new boolean[] { false, true, true, true, true, true, false }
-        );
+        ));
         // Duel 7 to 8
-        PartialSequenceBlueprint psb4 = new PartialSequenceBlueprint(
+        psbs.add(new PartialSequenceBlueprint(
             new boolean[] { false, true, true, true, true, true, true },
             1,
             false,
             new boolean[] { false, false, true, true, true, true, true, true, true, false, true, true, true, true, false }
-        );
+        ));
         // Duel 9 to 12 (ending the match)
-        PartialSequenceBlueprint psb5 = new PartialSequenceBlueprint(
+        psbs.add(new PartialSequenceBlueprint(
             new boolean[] { false, true, true, true, true, false, false },
             2,
             false,
             new boolean[] { false, false, false, false, false, false, true, true, true, false, false, true, true, true, false, false, false, false, false, false, false, true, true, true, true, true, false, false, false, true, true, true, true, true, true, true, false }
-        );
+        ));
 
-        psbs.clear();
-        psbs.add(psb1);
-        psbs.add(psb2);
-        psbs.add(psb3);
-        psbs.add(psb4);
-        psbs.add(psb5);
         mrb = new MatchResultBlueprint(
             -1,
             "Dummy 1",
@@ -127,6 +121,8 @@ public class SingleMatchCalculatorTest {
 
         assertEquals("Player 1 accuracy for match should be around 46,81%", 0.4681, player1Accuracy, 0.01);
         assertEquals("Player 2 accuracy for match should be around 58,14%", 0.5814, player2Accuracy, 0.01);
+
+        psbs.clear();
     }
 
 }
