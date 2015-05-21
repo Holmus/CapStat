0d1906df1dcce7af6bb2c73ee7c9d6e2236950df
Christian Persson <chrp@student.chalmers.se>
2015-05-21 17:10:35 +0200

    Introduces method to create partial sequences modelling complete duels

--- src/main/java/capstat/model/SingleMatchCalculator.java
+++ src/main/java/capstat/model/SingleMatchCalculator.java
@@ -1,5 +1,8 @@
 package capstat.model;
 
+import capstat.infrastructure.EventBus;
+import capstat.infrastructure.NotifyEventListener;
+import capstat.infrastructure.PartialSequenceBlueprint;
 import capstat.model.Match;
 import capstat.model.MatchFactory;
 import capstat.model.MatchResult;
@@ -18,7 +21,7 @@ public class SingleMatchCalculator {
     }
 
     /**
-     * Calculates the accuracy for the given player. The accuracy is given as a double d in the range 0 ≤ d ≤ 1.
+     * Calculates the accuracy for the given player. The accuracy is given as a double in the range [0, 1].
      * @param player the Player to calculate the accuracy for (Match.Player.ONE or Match.Player.TWO)
      * @return a double containing the accuracy
      */
@@ -41,6 +44,89 @@ public class SingleMatchCalculator {
 
     public int getTotalNumberOfThrows(Match.Player player) {
         return getThrowsForPlayer(mr.getSequences(), player).size();
+    }
+
+    // public static int getElapsedTime(MatchResult mr) {
+
+    // }
+
+    /**
+     * Takes an arbitrary list of {@link capstat.model.PartialSequenceResult} and "replays" them, giving back a new list of {@link capstat.model.PartialSequenceResult} with each instance containing the sequence from the throw after the last duel (or from the beginning of the match) up to and including the throw that ends the next duel.
+     * @param sequences the arbitrary list of sequences
+     * @return a {@link java.util.List} of {@link capstat.model.PartialSequenceResult}
+     */
+    public static List<PartialSequenceResult> getDuelSequences(List<PartialSequenceResult> sequences) {
+        List<PartialSequenceResult> duels = new ArrayList<>();
+        List<PartialSequenceBlueprint> duelBlueprints = new ArrayList<>();
+
+        List<Match.Throw> allThrows = getThrowsFromSequences(sequences);
+        List<Match.Throw> duelThrows = new ArrayList<>();
+
+        Match underlyingMatch = MatchFactory.createDefaultMatch();
+        underlyingMatch.startMatch();
+
+        NotifyEventListener listener = new NotifyEventListener() {
+
+            private PartialSequenceResult psr = sequences.get(0);
+            private boolean[] glasses = fromGlasses(psr.getGlasses());
+            private int startingPlayer = fromPlayer(psr.getStartingPlayer());
+            private boolean throwBeforeWasHit = psr.throwBeforeWasHit();
+
+            @Override
+            public void notify(String event) {
+                PartialSequenceBlueprint psb = new PartialSequenceBlueprint(
+                    glasses,
+                    startingPlayer,
+                    throwBeforeWasHit,
+                    fromThrows(duelThrows)
+                );
+                duelBlueprints.add(psb);
+
+                // Set the initial data for the next sequence
+                glasses = fromGlasses(underlyingMatch.getGlasses());
+                startingPlayer = fromPlayer(underlyingMatch.getPlayerWhoseTurnItIs());
+                throwBeforeWasHit = false;
+                duelThrows.clear();
+            }
+
+            private boolean[] fromGlasses(Match.Glass[] glasses) {
+                boolean[] bools = new boolean[glasses.length];
+                for (int i = 0; i < bools.length; i++) {
+                    bools[i] = glasses[i].isActive();
+                }
+                return bools;
+            }
+
+            private int fromPlayer(Match.Player player) {
+                return player == Match.Player.ONE ? 1 : 2;
+            }
+
+            private boolean[] fromThrows(List<Match.Throw> throwList) {
+                boolean[] bools = new boolean[throwList.size()];
+                for (int i = 0; i < bools.length; i++) {
+                    bools[i] = throwList.get(i) == Match.Throw.HIT ? true : false;
+                }
+                return bools;
+            }
+
+        };
+
+        EventBus eb = EventBus.getInstance();
+        eb.addNotifyEventListener(Match.DUEL_ENDED, listener);
+
+        for (Match.Throw t : allThrows) {
+            duelThrows.add(t);
+            if (t == Match.Throw.HIT)
+                underlyingMatch.recordHit();
+            else
+                underlyingMatch.recordMiss();
+        }
+
+        for (PartialSequenceBlueprint blueprint : duelBlueprints) {
+            duels.add(new PartialSequenceResult(blueprint));
+        }
+
+        return duels;
     }
 
     private static List<Match.Player> getPlayerTurnsFromSequences(List<PartialSequenceResult> sequences) {
