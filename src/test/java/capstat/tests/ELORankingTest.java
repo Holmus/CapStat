4e0cc3602f644a956355224291544f52c8b388fe
hjorthjort <rikard.hjort@gmail.com>
2015-05-22 15:26:47 +0200

    Adds tests for equals and hashcode

--- src/test/java/capstat/tests/ELORankingTest.java
+++ src/test/java/capstat/tests/ELORankingTest.java
@@ -23,4 +23,41 @@ public class ELORankingTest {
         ELORanking ranking = new ELORanking(-1000);
     }
 
+    @Test
+    public void equalsTest() {
+        ELORanking ranking = new ELORanking(1000);
+        assertFalse("Compared to null", ranking.equals(null));
+        assertFalse("Compared to object", ranking.equals(new Object()));
+        assertFalse("Compared to different ELORanking", ranking.equals(new
+                ELORanking(1500)));
+        assertFalse("Compared to subclass", ranking.equals(new ELORanking
+                (1000) {
+            //Subclass
+        }));
+
+        assertTrue("Compared to itself", ranking.equals(ranking));
+        assertTrue("Compared to new object, same ranking", ranking.equals(new
+                ELORanking(1000)));
+        assertTrue("Compared to new object, similar ranking", ranking.equals
+                (new ELORanking(1000.000999)));
+        assertTrue("Compared to new object, similar ranking", ranking.equals
+                (new ELORanking(999.99500001)));
+    }
+
+    @Test
+    public void hashCodeTest() {
+        ELORanking ranking = new ELORanking(1000);
+        assertTrue("Same ranking", ranking.hashCode() == new ELORanking(1000)
+                .hashCode());
+        assertTrue("Very similar ranking", ranking.hashCode() == new
+                ELORanking(1000.0001392).hashCode());
+        assertTrue("Very similar ranking", ranking.hashCode() == new
+                ELORanking(999.9951392).hashCode());
+
+        assertFalse("Different ranking", ranking.hashCode() == new ELORanking
+                (999).hashCode());
+        assertFalse("Different ranking", ranking.hashCode() == new ELORanking
+                (1001).hashCode());
+    }
+
 }
