66d99841e8df9e776c9190df3f3d9226903e8eb9
hjorthjort <rikard.hjort@gmail.com>
2015-05-22 15:26:00 +0200

    Creates unit tests for construction on ELO

--- src/test/java/capstat/tests/ELORankingTest.java
+++ src/test/java/capstat/tests/ELORankingTest.java
@@ -1,7 +1,26 @@
 package capstat.tests;
 
+import capstat.model.ELORanking;
+import capstat.model.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author hjorthjort
  */
 public class ELORankingTest {
+
+    @Test
+    public void constructionTest() {
+        ELORanking ranking = new ELORanking(1000);
+        assertTrue(ranking.getPoints() == 1000.00);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructionWithExceptionTest() {
+        ELORanking ranking = new ELORanking(-1000);
+    }
+
 }
