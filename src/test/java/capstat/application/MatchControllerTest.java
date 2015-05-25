d2d544718a13c80003865e1b2036c96b7f749e40
hjorthjort <rikard.hjort@gmail.com>
2015-05-25 14:18:33 +0200

    Creates skeleton for matchcontroller test

---
+++ src/test/java/capstat/application/MatchControllerTest.java
@@ -0,0 +1,57 @@
+package capstat.application;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author hjorthjort
+ */
+public class MatchControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateNewMatch() throws Exception {
+
+    }
+
+    @Test
+    public void testStartMatch() throws Exception {
+
+    }
+
+    @Test
+    public void testRecordMiss() throws Exception {
+
+    }
+
+    @Test
+    public void testRecordHit() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer1() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer2() throws Exception {
+
+    }
+
+    @Test
+    public void testSetEndGameStrategy() throws Exception {
+
+    }
+
+    @Test
+    public void testNotifyEvent() throws Exception {
+
+    }
+}
