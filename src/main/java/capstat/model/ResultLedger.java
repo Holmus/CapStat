7d34f74991af9f6efac0e80b89b96df822e5e360
hjorthjort <rikard.hjort@gmail.com>
2015-05-26 18:37:02 +0200

    Adds helper methods for creating blueprints in ResultsLedger

--- src/main/java/capstat/model/ResultLedger.java
+++ src/main/java/capstat/model/ResultLedger.java
@@ -1,7 +1,14 @@
 package capstat.model;
 
+import capstat.infrastructure.DatabaseHelperFactory;
 import capstat.infrastructure.MatchDatabaseHelper;
+import capstat.infrastructure.MatchResultBlueprint;
+import capstat.infrastructure.PartialSequenceBlueprint;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,7 +17,55 @@ import java.util.Map;
  * the domain and delegates the issue of persistant sotrage of Match objects.
  */
 public class ResultLedger {
-	private static UserLedger instance;
+	private static UserLedger instance = UserLedger.getInstance();
 	private Map<String, User> users;
 	private MatchDatabaseHelper dbHelper;
+
+	private ResultLedger() {
+		this.users = new HashMap<>();
+		this.dbHelper = new DatabaseHelperFactory().createMatchQueryHelper();
+	}
+
+	public void registerResult(Match match) {
+//TODO:implemnet
+	}
+
+	public MatchResultBlueprint createBlueprint(MatchResult result) {
+		List<PartialSequenceBlueprint> sequenceBlueprintList = new
+				LinkedList<>();
+		for (PartialSequenceResult sequence : result.getSequences()) {
+			sequenceBlueprintList.add(createBlueprint(sequence));
+		}
+		return new MatchResultBlueprint(result.getId(),
+				result.getPlayer1().getNickname(),
+				result.getPlayer2().getNickname(),
+				result.getSpectator().getNickname(),
+				result.getPlayer1score(),
+				result.getPlayer2score(),
+				result.getStartTime().toEpochMilli(),
+				result.getEndTime().toEpochMilli(),
+				sequenceBlueprintList);
+	}
+
+	public PartialSequenceBlueprint createBlueprint(PartialSequenceResult
+															sequence) {
+		//Translate glasses array to boolean array
+		Match.Glass[] glasses = sequence.getGlasses();
+		boolean[] newGlasses = new boolean[glasses.length];
+		for (int i = 0; i < glasses.length; i++) {
+			newGlasses[i] = glasses[i].isActive();
+		}
+
+		//Translate throws array to boolean array.
+		Match.Throw[] partialSequence = sequence.getSequence();
+		boolean[] newPartialSequence = new boolean[partialSequence.length];
+		for (int i = 0; i < partialSequence.length; i++) {
+			newPartialSequence[i] = partialSequence[i] == Match.Throw.HIT;
+		}
+
+		return new PartialSequenceBlueprint(newGlasses,
+				sequence.getStartingPlayer().ordinal() + 1,
+				sequence.throwBeforeWasHit(),
+				newPartialSequence);
+	}
 }
