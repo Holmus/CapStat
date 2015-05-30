c95dd550f5c4cefd39c1bd26c1b03186fe2b5670
hjorthjort <rikard.hjort@gmail.com>
2015-05-30 02:04:33 +0200

    Adds more methods to the statistics controller

--- src/main/java/capstat/application/StatisticsController.java
+++ src/main/java/capstat/application/StatisticsController.java
@@ -4,9 +4,10 @@ import capstat.model.statistics.*;
 import capstat.model.user.User;
 
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.logging.Filter;
+import java.util.stream.Stream;
 
 /**
  * @author hjorthjort
@@ -16,9 +17,7 @@ import java.util.Set;
  * This class has a number of constants, representing implementations of
  * Statistic for some common statistic. A client may also use their own
  * implementation of Statistic to obtain a list of Plottables for a set of
- * Matches. The getData method requests Plottables from the Statistic it gets
- * passed as parameter with a list of all the given user's matches, sorted by
- * their end time, from the earliest to the latest.
+ * Matches.
  *
  */
 public class StatisticsController {
@@ -29,13 +28,56 @@ public class StatisticsController {
     public static final Statistic NUMBER_OF_THROWS = new NumberOfThrows();
     public static final Statistic ELAPSED_TIME = new MatchDuration();
 
+    /**
+     * This method uses the full getData method, sorting the match
+     * results by their end time and filtering them with a predicate
+     * that is always true, which means the full unfiltered list of a users
+     * matches is used.
+     */
     public List<Plottable> getData(Statistic dataType, User user) {
-        Set<MatchResult> matches = ResultLedger.getInstance()
-                .getMatchesForUser(user);
-        List<MatchResult> newList = new LinkedList<>(matches);
-        newList.sort((match1, match2) -> match1.getEndTime().compareTo(match2
-                .getEndTime()));
-        return dataType.getStatistics(newList, user);
+        Comparator<MatchResult> comparator = (m1, m2) -> m1.getEndTime()
+                .compareTo(m2.getEndTime());
+        return getData(dataType, user, comparator);
     }
 
+    /**
+     * This method uses the full getData method with a predicate that is
+     * always true, which means the full unfiltered list of a users matches
+     * is returned.
+     */
+    public List<Plottable> getData(Statistic dataType, User user,
+                                   Comparator<? super MatchResult>
+                                           sortingComparator) {
+        return getData(dataType, user, sortingComparator, (a) -> true);
+    }
+
+    /**
+     * This method requests Plottables from the Statistic it gets passed as a
+     * parameter with a list of all the given users matches, sorted by the
+     * order defined by the comparator passed as argument and filterd using
+     * the predicate passed as argument
+     * @param dataType the statstic used for creating plottables
+     * @param user the user for which to fetch statistics
+     * @param sortingComparator comparator determining the order of the List
+     *                          that is passed to the Statistic.
+     * @param filter a predicate by which to filter the set of matches to be
+     *               used.
+     * @return a list of plottables as specified by the concrete
+     * implementation of dataType.
+     */
+    public List<Plottable> getData(Statistic dataType, User user,
+                                   Comparator<? super MatchResult>
+                                           sortingComparator,
+                                   Predicate<? super MatchResult> filter) {
+        Set<MatchResult> matches = ResultLedger.getInstance()
+                .getMatchesForUser(user);
+        Stream<MatchResult> stream = matches.stream().filter(filter).sorted
+                (sortingComparator);
+        List<MatchResult> newList = new LinkedList<>();
+        Iterator<MatchResult> iterator = stream.iterator();
+        while (iterator.hasNext()) {
+            newList.add(iterator.next());
+        }
+        return dataType.getStatistics(newList, user);
+    }
 }
