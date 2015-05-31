Holmus/CapStat

commit 7d3b520fcde4f360eb3041ad313d2cb3a4364c29

Fixes bug where throw sequences would have the wrong values and be added twice to database

src/main/java/capstat/model/match/ThrowSequence.java
@@ -53,7 +53,7 @@ public class ThrowSequence {
     public ThrowSequence(ThrowSequence ts) {
         //Get a deep getCopy of the sequences list
         this.sequences = new LinkedList<>(ts.getSequences());
-        this.currentSequence = ts.currentSequence.getCopy();
+        this.currentSequence = this.sequences.pop();
         this.undoStack = (Stack<Match.Throw>)ts.undoStack.clone();
     }
 
@@ -139,6 +139,10 @@ public class ThrowSequence {
         return clone;
     }
 
+    public void changeStartingPlayerForCurrentSequence(final Match.Player player) {
+        this.currentSequence.startingPlayer = player;
+    }
+
     /**
      * Objects of this class are immutable, except for by instances of
      * ThrowSequence.
@@ -151,17 +155,26 @@ public class ThrowSequence {
 
         private PartialSequence(Match.Glass[] glasses, Match.Player
                 startingPlayer, boolean throwBeforeWasHit) {
-            this.glasses = glasses;
+            this.glasses = cloneGlasses(glasses);
             this.startingPlayer = startingPlayer;
             this.throwBeforeWasHit = throwBeforeWasHit;
             this.sequence = new LinkedList<>();
         }
 
         private PartialSequence(final PartialSequence partialSequence) {
-            this.glasses = partialSequence.glasses.clone();
+            this.glasses = cloneGlasses(partialSequence.glasses);
             this.startingPlayer = partialSequence.startingPlayer;
             this.throwBeforeWasHit = partialSequence.throwBeforeWasHit;
             this.sequence = new LinkedList<>(partialSequence.sequence);
+        }
+
+        private Match.Glass[] cloneGlasses(Match.Glass[] array) {
+            Match.Glass[] ret = new Match.Glass[array.length];
+            for (int i = 0; i < array.length; i++) {
+                ret[i] = new Match.Glass();
+                ret[i].setActive(array[i].isActive());
+            }
+            return ret;
         }
 
         private void add(Match.Throw newThrow) {

src/main/java/capstat/model/match/Match.java
@@ -162,6 +162,14 @@ public class Match {
      */
     public void setCurrentPlayer(Player player) {
         this.playerWhoseTurnItIs = player;
+        //If we are at the beginning of a sequence, just change the player
+        // whos turn it is. But if we are not, start a new partial sequence
+        if (this.throwSequence.canRewind()) {
+            this.throwSequence.updateRecordState(glasses, playerWhoseTurnItIs,
+                    throwSequence.lastThrowWasHit());
+        } else {
+            this.throwSequence.changeStartingPlayerForCurrentSequence(player);
+        }
     }
 
     //Getters

commit 0bacd303816087df0795e9eb097be87861725524

Fixes bug that was introduced by 7d3b520

src/main/java/capstat/model/match/ThrowSequence.java
@@ -53,7 +53,7 @@ public class ThrowSequence {
     public ThrowSequence(ThrowSequence ts) {
         //Get a deep getCopy of the sequences list
         this.sequences = new LinkedList<>(ts.getSequences());
-        this.currentSequence = this.sequences.pop();
+        this.currentSequence = this.sequences.pollLast().getCopy();
         this.undoStack = (Stack<Match.Throw>)ts.undoStack.clone();
     }
 