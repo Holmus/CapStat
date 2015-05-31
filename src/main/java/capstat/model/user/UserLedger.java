0037930a7751ee45c98b2a1d9fdf9db70792c2cf
hjorthjort <rikard.hjort@gmail.com>
2015-05-31 16:55:33 +0200

    Slight refactoring decoupling LoggedInUser from UserFactory

--- src/main/java/capstat/model/user/UserLedger.java
+++ src/main/java/capstat/model/user/UserLedger.java
@@ -42,6 +42,10 @@ public class UserLedger {
         return instance;
     }
 
+    public static User getGuestUser() {
+        return UserFactory.createGuestUser();
+    }
+
     //Retrieval
 
     /**
